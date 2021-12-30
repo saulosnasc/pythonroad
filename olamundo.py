2ce1ed5cee66fc64a8ab5ca2ac77922ebceeb454
Saulo Nascimento <saulo.snasc@gmail.com>
2021-12-30 15:50:11 -0300

    Testando como funcionam os comentários

--- olamundo.py
+++ olamundo.py
@@ -1,1 +1,7 @@
 print("Olá mundo!")
+
+
+# -------------------------------
+Inserindo comentários no programa para testar como eles funcionam!
+Comentário inserido por Saulo da Silva Nascimento
+# -------------------------------
